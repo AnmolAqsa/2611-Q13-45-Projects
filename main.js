37747d44c9c63f9cbbe615a273dbedfa0c05f202
AnmolAqsa <165940283+AnmolAqsa@users.noreply.github.com>
2024-07-04 12:33:40 -0700

    Add files via upload
    Q13, 45 Projects

---
+++ main.js
@@ -0,0 +1,6 @@
+"use strict";
+const myTransport = ["Bike", "Train", "Bus", "Aroplane"];
+for (let i = 0; i < myTransport.length; i++) {
+    console.log(`I would like to own a ${myTransport[i]}`);
+}
+;
